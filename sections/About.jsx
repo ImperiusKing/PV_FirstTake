d1ac17fad1b1ed5ce6f17d1bee44d914dbd9206d
ImperiusKing <stapgold.fsc@gmail.com>
2023-08-23 02:51:36 +0700

    add: autonumber

--- sections/About.jsx
+++ sections/About.jsx
@@ -16,13 +16,14 @@ const About = () => (
       viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
     >
-      <TypingText title="| About Metaversus" textStyles="text-center" />
+      <TypingText title="| About PV achievement " textStyles="text-center" />
 
       <motion.p
         variants={fadeIn('up', 'tween', 0.2, 1)}
         className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
       >
-        <span className="font-extrabold text-white">Metaverse</span> is a new
+        <span className="font-extrabold text-white">PV</span> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        {/* <span className="font-extrabold text-white">Metaverse</span> is a new
         thing in the future, where you can enjoy the virtual world by feeling
         like it's really real, you can feel what you feel in this metaverse
         world, because this is really the{' '}
@@ -34,15 +35,36 @@ const About = () => (
         easily explore the metaverse world you want, turn your dreams into
         reality. Let's{' '}
         <span className="font-extrabold text-white">explore</span> the madness
-        of the metaverse by scrolling down
+        of the metaverse by scrolling down */}
       </motion.p>
+        <div class="wrapper">
+          <div class="container">
+            <span class="num" data-val="50">000</span>
+            <span class="text"> Meals</span>
+          </div>
+          <div class="container">
+            <span class="num" data-val="100">000</span>
+            <span class="text"> Meals</span>
+          </div>
+          <div class="container">
+            <span class="num" data-val="200">000</span>
+            <span class="text"> Meals</span>
+          </div>
+          <div class="container">
+            <span class="num" data-val="10">000</span>
+            <span class="text"> Meals</span>
+          </div>
+        </div>
 
-      <motion.img
+        <script srs="../utils/autonumber.js">
+
+        </script>
+      {/* <motion.img
         variants={fadeIn('up', 'tween', 0.3, 1)}
         src="/arrow-down.svg"
         alt="arrow down"
         className="w-[18px] h-[28px] object-contain mt-[28px]"
-      />
+      /> */}
     </motion.div>
   </section>
 );
